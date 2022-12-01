3513e0c6654846646b589fa8c198203dc2753b85
Arun Appulingam <arunapp13@outlook.com>
2022-12-01 11:11:48 +0000

    fix missing app lines

--- prod_rev_analysis/streamlit/app.py
+++ prod_rev_analysis/streamlit/app.py
@@ -1,12 +1,14 @@
 import requests
 import streamlit as st
- # from streamlit_lottie import st_lottie
-import pandas as pd
+# from streamlit_lottie import st_lottie
 import time
 import datetime
 import requests
 from PIL import Image
 import base64
+import pandas as pd
+
+
 
 st.set_page_config(page_title="My Webpage", page_icon= "tada", layout= "wide")
 
@@ -23,7 +25,6 @@ with st.container():
         data_url = base64.b64encode(contents).decode('utf-8-sig')
         file.close()
         st.markdown(f'<img src="data:image/gif;base64,{data_url}">',unsafe_allow_html = True)
-
 st.write("---")
 st.markdown("# Introduction 📈")
 st.sidebar.markdown("# Page 1: 📈")
@@ -50,13 +51,13 @@ def load_data():
 
 review_data=load_data()
 st.markdown("#### Step 1:")
-url = st.text_input("** `Give the URL link :` **", None)
+url = st.text_input("**`Give the URL link:`**", None)
 
 st.write("Or")
-company_name = st.text_input("** `Give the CompanyID :` **", None)
+company_name = st.text_input("**`Give the CompanyID:`**", None)
 
 st.markdown("#### Step 2:")
-st.write("** `Get Review From :` **")
+st.write("**`Get Review From :`**")
 form = st.form("form", clear_on_submit=True)
 with form:
     st.markdown("<h2 style='text-align: center;'>Choose One:</h2>",unsafe_allow_html= True)
@@ -67,12 +68,12 @@ with form:
 
     with column2:
         st.image('/Users/arun._.appulingam/code/rsz_1yelp-image.png')
-     #     column2.write(f"`Yelp`")
+    #     column2.write(f"`Yelp`")
         yelp = column2.checkbox('Yelp')
 
     with column3:
         st.image('/Users/arun._.appulingam/code/rsz_602e2fe1d9ced200045a5771.png')
-     #     column3.write('')
+    #     column3.write('')
         trust_pilot = column3.checkbox('TrustPilot')
 
     flag = True
@@ -99,20 +100,73 @@ with c2:
     """
     st.markdown(button_style,unsafe_allow_html=True)
 
- # if url is None:
- #     "You missed out information"
- # elif form is None:
- #     return "You missed out information"
- # else:
- #     return x
+# if url is None:
+#     "You missed out information"
+# elif form is None:
+#     return "You missed out information"
+# else:
+#     return x
 
- # if submit:
- #     list_values = [int(i) for i in values]
- #     if sum(list_values) == 0:
- #         st.error("**Choose an option!**")
- #         time.sleep(1)
- #         st.experimental_rerun()
+# if submit:
+#     list_values = [int(i) for i in values]
+#     if sum(list_values) == 0:
+#         st.error("**Choose an option!**")
+#         time.sleep(1)
+#         st.experimental_rerun()
 
 
- # def get_total_dataframe(dataset):
- #     total_dataframe = pd.DataFrame({
+# def get_total_dataframe(dataset):
+#     total_dataframe = pd.DataFrame({
+#     'Status':['Confirmed', 'Recovered', 'Deaths','Active'],
+#     'Number of cases':(dataset.iloc[0]['confirmed'],
+#     dataset.iloc[0]['recovered'],
+#     dataset.iloc[0]['deaths'],dataset.iloc[0]['active'])})
+#     return total_dataframe
+
+# state_total = get_total_dataframe(state_data)
+
+# if st.sidebar.checkbox("Show Analysis by State", True, key=2):
+#     st.markdown("## **State level analysis**")
+#     st.markdown("### Overall Confirmed, Active, Recovered and " +
+#     "Deceased cases in %s yet" % (select))
+#     if not st.checkbox('Hide Graph', False, key=1):
+#         state_total_graph = px.bar(
+#         state_total,
+#         x='Status',
+#         y='Number of cases',
+#         labels={'Number of cases':'Number of cases in %s' % (select)},
+#         color='Status')
+#         st.plotly_chart(state_total_graph)
+
+
+
+
+# with st.form(key='params_for_api'):
+
+#     pickup_date = st.date_input('pickup datetime', value=datetime.datetime(2012, 10, 6, 12, 10, 20))
+#     pickup_time = st.time_input('pickup datetime', value=datetime.datetime(2012, 10, 6, 12, 10, 20))
+#     pickup_datetime = f'{pickup_date} {pickup_time}'
+#     pickup_longitude = st.number_input('pickup longitude', value=40.7614327)
+#     pickup_latitude = st.number_input('pickup latitude', value=-73.9798156)
+#     dropoff_longitude = st.number_input('dropoff longitude', value=40.6413111)
+#     dropoff_latitude = st.number_input('dropoff latitude', value=-73.7803331)
+#     passenger_count = st.number_input('passenger_count', min_value=1, max_value=8, step=1, value=1)
+
+#     st.form_submit_button('Make prediction')
+
+# params = dict(
+#     pickup_datetime=pickup_datetime,
+#     pickup_longitude=pickup_longitude,
+#     pickup_latitude=pickup_latitude,
+#     dropoff_longitude=dropoff_longitude,
+#     dropoff_latitude=dropoff_latitude,
+#     passenger_count=passenger_count)
+
+# wagon_cab_api_url = 'https://taxifare.lewagon.ai/predict'
+# response = requests.get(wagon_cab_api_url, params=params)
+
+# prediction = response.json()
+
+# pred = prediction['fare']
+
+# st.header(f'Fare amount: ${round(pred, 2)}')
