900f91cea19d84263b6b125404c564554cf4f6f4
mari khomeriki <khomeriki.mari@gmail.com>
2022-12-02 17:07:39 +0000

    packaging v1

--- prod_rev_analysis/interface/main.py
+++ prod_rev_analysis/interface/main.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 
 from colorama import Fore, Style
+from prod_rev_analysis.ml_logic.registry import load_model
+
+from prod_rev_analysis.ml_logic.data import load_data, clean_data
 
 
 
@@ -11,55 +14,24 @@ from colorama import Fore, Style
 # from taxifare.ml_logic.params import CHUNK_SIZE, DATASET_SIZE, VALIDATION_DATASET_SIZE
 # from taxifare.ml_logic.preprocessor import preprocess_features
 # from taxifare.ml_logic.registry import load_model, save_model
+from collections import Counter
 
 
-def preprocess(source_type = 'train'):
-    """
-    Preprocess the dataset by chunks fitting in memory.
-    parameters:
-    - source_type: 'train' or 'val'
-    """
-
-    print("\n⭐️ Use case: preprocess")
-    # CODE here
-
-
-def train():
-    """
-    Train a new model on the full (already preprocessed) dataset ITERATIVELY, by loading it
-    chunk-by-chunk, and updating the weight of the model after each chunks.
-    Save final model once it has seen all data, and compute validation metrics on a holdout validation set
-    common to all chunks.
-    """
-    print("\n⭐️ Use case: train")
-     # CODE here
-
-
-def evaluate():
-    """
-    Evaluate the performance of the latest production model on new data
-    """
-    print("\n⭐️ Use case: evaluate")
-     # CODE here
-
-
-def pred(X_pred: pd.DataFrame = None) ->np.ndarray:
+def pred():
     """
     Make a prediction using the latest trained model
     """
+    data = load_data()
+    data_cleaned = clean_data(data)
+    model = load_model()
+    y_pred =  model.predict(data_cleaned)
+    y_pred = [0 if pred <0.5 else 1 for pred in y_pred]
+    y_pred = ['Negative' if sent == 0 else 'Positive' for sent in y_pred]
 
-    print("\n⭐️ Use case: predict")
-     # CODE here
-
-
-
-
+    return Counter(y_pred)
 
 
 if __name__ == '__main__':
-    # preprocess()
-    # preprocess(source_type='val')
-    # train()
-    # pred()
-    # evaluate()
-    pass
+
+    result = pred()
+    print(result)
--- prod_rev_analysis/ml_logic/data.py
+++ prod_rev_analysis/ml_logic/data.py
@@ -1,43 +1,48 @@
-from taxifare.ml_logic.params import (COLUMN_NAMES_RAW,
-                                            DTYPES_RAW_OPTIMIZED,
-                                            DTYPES_RAW_OPTIMIZED_HEADLESS,
-                                            DTYPES_PROCESSED_OPTIMIZED
-                                            )
+# from taxifare.ml_logic.params import (COLUMN_NAMES_RAW,
+#                                             DTYPES_RAW_OPTIMIZED,
+#                                             DTYPES_RAW_OPTIMIZED_HEADLESS,
+#                                             DTYPES_PROCESSED_OPTIMIZED
+#                                             )
 
-from taxifare.data_sources.local_disk import (get_pandas_chunk, save_local_chunk)
+# from taxifare.data_sources.local_disk import (get_pandas_chunk, save_local_chunk)
 
-from taxifare.data_sources.big_query import (get_bq_chunk, save_bq_chunk)
+# from taxifare.data_sources.big_query import (get_bq_chunk, save_bq_chunk)
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import pandas as pd
+import numpy as np
+from tensorflow.keras.preprocessing.text import text_to_word_sequence
+import pickle
+
 
 import os
-import pandas as pd
 
 
-def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+def load_data():
+    # df = pd.read_csv('/Users/marikhomeriki/code/marikhomeriki/raw_data/data_dishoom.csv', header=None)
+    df = pd.read_csv('../review.csv', header=None)
+    df = df.rename({1: 'text'}, axis = 1)
+    df = df.dropna()
+    df = pd.DataFrame(df.text)
+
+    train_sentences = df['text']
+
+
+    X = [text_to_word_sequence(_) for _ in train_sentences]
+
+    return X
+
+def clean_data(list) -> np.ndarray:
     """
-    clean raw data by removing buggy or irrelevant transactions
-    or columns for the training set
+    clean raw data by tokenizing and padding the reviews
     """
+    with open('tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
 
-    # remove useless/redundant columns
-    df = df.drop(columns=['key'])
+    df_token = tokenizer.texts_to_sequences(list)
 
-    # remove buggy transactions
-    df = df.drop_duplicates()  # TODO: handle in the data source if the data is consumed by chunks
-    df = df.dropna(how='any', axis=0)
-    df = df[(df.dropoff_latitude != 0) | (df.dropoff_longitude != 0) |
-            (df.pickup_latitude != 0) | (df.pickup_longitude != 0)]
-    df = df[df.passenger_count > 0]
-    df = df[df.fare_amount > 0]
+    df_token_pad = pad_sequences(df_token, maxlen=50, dtype='float32', padding="post")
 
-    # remove irrelevant/non-representative transactions (rows) for a training set
-    df = df[df.fare_amount < 400]
-    df = df[df.passenger_count < 8]
-    df = df[df["pickup_latitude"].between(left=40.5, right=40.9)]
-    df = df[df["dropoff_latitude"].between(left=40.5, right=40.9)]
-    df = df[df["pickup_longitude"].between(left=-74.3, right=-73.7)]
-    df = df[df["dropoff_longitude"].between(left=-74.3, right=-73.7)]
 
-    print("\n✅ data cleaned")
-
-    return df
-
+    return df_token_pad
